c10607796157fec8e6d5c9104aacfc348be3425a
ndgndg91 <ndgndg91@gmail.com>
2021-01-09 17:25:05 +0900

    <chore> shortcut prod environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  dashboard: 'https://master.d1et269uohw2tu.amplifyapp.com'
+  dashboard: 'http://my.new.shortcut.s3-website.ap-northeast-2.amazonaws.com'
 };
