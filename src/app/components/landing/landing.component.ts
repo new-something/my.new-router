31fbbdab36a1f07436b17983f8837cab43b4cf84
ndgndg91 <ndgndg91@gmail.com>
2021-01-25 20:13:51 +0900

    <feat> google signin

--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -7,29 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingComponent implements OnInit {
 
-  constructor() { }
+  public constructor() { }
 
-  ngOnInit(): void {
+  public ngOnInit(): void {
     // my.new 도메인 입력하여 방문 시 로그인 되어있지 않은 사용자가 바로 로그인하게 유도한다 (구글 .new 도메인 정책)
     this.openSignInPopup();
   }
 
-  openSignInPopup(): void {
+  public openSignInPopup(): void {
     document.querySelector('#signinPopup').classList.add('is-active');
   }
 
-  closeSignInPopup(): void {
+  public closeSignInPopup(): void {
     document.querySelector('#signinPopup').classList.remove('is-active');
   }
 
-  changeDestination(event: any): void {
+  public changeDestination(event: any): void {
     const button = document.getElementById('buttonOpenInNewTab');
     button.setAttribute('href', '/' + event.target.value);
   }
 
-  startSignIn(): void {
+  public startGithubSignIn(): void {
     window.location.href = 'https://github.com/login/oauth/authorize?scope=read:user&client_id=6d91f0584d549619c938';
-    // window.location.href = this.dashboardUrl;
   }
 
+  public startGoogleSignIn(): void {
+    window.location.href = 'https://accounts.google.com/o/oauth2/v2/auth?client_id=737679013674-q9efp32jd44hu4gmetvavqr64d9rj97h.apps.googleusercontent.com&response_type=code&state=state_parameter_passthrough_value&scope=https%3A//www.googleapis.com/auth/userinfo.profile&redirect_uri=http%3A//localhost:8080/users/google&prompt=consent&include_granted_scopes=true';
+  }
 }
