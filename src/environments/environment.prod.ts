e007abb7474aa178b9c12ad7346b40df966a51bf
ndgndg91 <ndgndg91@gmail.com>
2021-04-01 22:53:30 +0900

    <chore> change userService

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-  userService: 'https://user.my.new',
+  userService: 'https://app.my.new',
+  // userService: 'https://user.my.new',
   appService: 'https://app.my.new',
   shortcut: 'https://shortcut.my.new'
 };
