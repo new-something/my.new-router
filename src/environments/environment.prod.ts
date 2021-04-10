b2ae28368fa34cea110f24ae7c3cae6f4e391a32
ndgndg91 <ndgndg91@gmail.com>
2021-04-11 00:15:40 +0900

    <chore> apply github login api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   userService: 'https://app.my.new',
-  userServerlessService: 'https://ocdaiir7mf.execute-api.us-east-1.amazonaws.com/test',
+  userServerlessService: 'https://iktltf58kh.execute-api.us-east-1.amazonaws.com/test',
   appService: 'https://app.my.new',
   shortcut: 'https://shortcut.my.new'
 };
